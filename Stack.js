2c3bf23a6477bcc0ec2ff336281b50ae74f50ccb
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-06-28 16:58:04 -0400

    stack terms

--- Stack.js
+++ Stack.js
@@ -12,13 +12,13 @@
 
 // 3. Write a method to peek() at the top of the stack
 
-// 4. Write a method to add() a node to the top of the stack
+// 4. Write a method to push() a node onto the top of the stack
 // The new node is the head of the stack
 // Set the old head to the .next propery of the new head
 // Increment the length of the stack
 // Edge case - what is different if the stack is empty
 
-// 5. Write a method to remove() a node from the top the stack
+// 5. Write a method to pop() a node off of the top the stack
 // Remove the node from the head of the stack (first in last out)
 // Set the old head's .next node to be the new head
 // Decrement the length of the stack
