96efb7f2ec50019da387a8af5df5a119efc32b17
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-06-28 16:34:34 -0400

    stack and queue

---
+++ Queue.js
@@ -0,0 +1,30 @@
+// Follow the steps to create a queue
+// Queues are first in, first out - like a line at a grocery store
+// Adding or removing items from a queue should *not* require traversing the queue
+
+// 1. Create Node class
+// Node constructor takes one arg (data) and has 3 properties: this.data, this.prev, & this.next
+// this.next & this.prev default to null
+
+// 2. Create Queue Class
+// Queue constructor takes no args
+// Queue constructor has 3 properties: this.head = null, this.tail = null & this.length = 0
+
+// 3. Write a method to getHead() of the queue
+
+// 4. Write a method to getTail() of the queue
+
+// 4. Write a method to add() a node to the head of the queue
+// The new node is the head of the queue
+// Set the old head to the .next propery of the new head
+// Increment the length of the queue
+// Edge case - what is different if the queue is empty?
+
+// 5. Write a method to remove() a node from the tail the queue
+// Remove the node from the tail of the queue (first in first out)
+// Set the old tail's .prev node to the new tail
+// Set the new tail's .next node to be the null
+// Decrement the length of the queue
+
+// 6. Write a method to print() the data in each node of the queue
+// Traverse the queue, console.log() each node's .data property
---
+++ Stack.js
@@ -0,0 +1,27 @@
+// Follow the steps to create a stack
+// Stacks are first in, last out - like a stack of dishes
+// Adding or removing items from a stack should *not* require traversing the stack
+
+// 1. Create Node class
+// Node constructor takes one arg (data) and has two properties: this.data & this.next
+// this.next defaults to null
+
+// 2. Create Stack Class
+// Stack constructor takes no args
+// Stack constructor has two properties: this.head = null & this.length = 0
+
+// 3. Write a method to peek() at the top of the stack
+
+// 4. Write a method to add() a node to the top of the stack
+// The new node is the head of the stack
+// Set the old head to the .next propery of the new head
+// Increment the length of the stack
+// Edge case - what is different if the stack is empty
+
+// 5. Write a method to remove() a node from the top the stack
+// Remove the node from the head of the stack (first in last out)
+// Set the old head's .next node to be the new head
+// Decrement the length of the stack
+
+// 6. Write a method to print() the data in each node of the stack
+// Traverse the stack, console.log() each node's .data property
